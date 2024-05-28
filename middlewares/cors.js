5cda14594cd6951e6e44ac82e100e947571c5def
rukaterin <maylessamurai@yandex.ru>
2024-05-28 21:07:13 +0300

    new cors2

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const allowedCors = [
     'https://students-projects.ru',
     'http://localhost:3000',
     'http://localhost:3001',
-    "https://pin-frontend.nomorepartiesco.ru"
+    "https://pin-front.nomorepartiesco.ru"
   ];
 
   function cors(req, res, next) {
